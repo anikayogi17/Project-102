e5f78311d5a0b9624102335be8515375d88fdede
Anika Yogi <80984914+anikayogi17@users.noreply.github.com>
2023-03-06 23:25:45 -0800

    Add files via upload

---
+++ Project102/Move_File.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+from_dir
+to_dir
+
+list_of_files = os.listdir(from_dir)
+//print(list_of_files)
+
+python Move_File.python
+
+for list_of_files == os.listdir(from_dir)
+
+if list_of_files == " "
+
